eccec4657fddce79b04a5b6be3d056b905856c74
unknown <bouncer4free@web.de>
2023-10-09 14:20:43 +0200

    fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements/dev.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [ $CREATE_SUPERUSER ];
+if [[ $CREATE_SUPERUSER ]];
 then
   python manage.py createsuperuser --no-input
 fi
