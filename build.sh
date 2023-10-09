425e23b695e3a0aced52c6ecb8b698eb06dbdde3
unknown <bouncer4free@web.de>
2023-10-09 14:03:53 +0200

    daphne

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 set -o errexit  # exit on error
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
 
 pip install -r requirements/dev.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
