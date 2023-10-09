dedf8549f2ba43fd3f8d69888ba67f94bfe5f98a
unknown <bouncer4free@web.de>
2023-10-09 17:23:10 +0200

    test

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 set -o errexit  # exit on error
 
+pip install wheel
 pip install -r requirements/dev.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
 
 if [[ $CREATE_SUPERUSER ]];
 then
