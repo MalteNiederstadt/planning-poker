f4b12aa0ec328da3cc40e23c7e2ac9a0c2a6c00b
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 09:05:56 +0200

    test2

--- example/asgi.py
+++ example/asgi.py
@@ -6,13 +6,22 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
 import django
+import chat.routing
 
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django.setup()
+#django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
 application = get_asgi_application()
 
 
