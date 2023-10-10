5b8c48471367f19aeb58c98a66ef14d241fa5a30
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 08:09:23 +0200

    rebuild

--- planning_poker_jira/models.py
+++ planning_poker_jira/models.py
@@ -8,8 +8,8 @@ from django.utils.translation import gettext_lazy as _
 from encrypted_fields import fields
 from jira import JIRA
 
-from planning_poker.models import PokerSession, Story
-
+#from planning_poker.models import PokerSession, Story
+from .base_models import PokerSession, Story
 logger = logging.getLogger(__name__)
 
 
