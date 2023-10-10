fa4f42113efb192dedb5f3ed79c523ce2ed7cb7e
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 10:23:42 +0200

    test

--- example/asgi.py
+++ example/asgi.py
@@ -1,17 +1,21 @@
-import os
+import os 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import get_default_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
-import django
 import example.routing
 
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django.setup()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
 application = ProtocolTypeRouter({
