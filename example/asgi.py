4aa831a924048a772c7efa0ab26e876976d4385e
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 08:58:17 +0200

    test2

--- example/asgi.py
+++ example/asgi.py
@@ -5,6 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
+import django
 
 
 # Initialize Django ASGI application early to ensure the AppRegistry
