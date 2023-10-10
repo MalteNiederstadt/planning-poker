51cd50cc15c602f864ea3014820f7daff4543792
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 12:01:49 +0200

    test

--- example/settings.py
+++ example/settings.py
@@ -126,7 +126,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(0.0.0.0, 6379)],
+            "hosts": [("0.0.0.0", 6379)],
         },
     },
 }
