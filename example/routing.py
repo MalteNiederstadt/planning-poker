d3223ca201626789ee67291b945c95c1493b86d3
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 14:10:48 +0200

    test2

--- example/routing.py
+++ example/routing.py
@@ -1,7 +1,14 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import planning_poker.routing
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/.*$', consumers.YourConsumer.as_asgi()),  # Wildcard path for all WebSocket connections
+]
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(URLRouter(planning_poker.routing.websocket_urlpatterns)),
 })
+
