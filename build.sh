9a2ebbcb25cdb293a7b91f8bae791ef2bde076c5
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-10 10:25:58 +0200

    test2

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -o errexit  # exit on error
 pip install wheel
 pip install -r requirements/dev.txt
 python manage.py makemigrations
-python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py migrate
 
