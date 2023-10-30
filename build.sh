88a2f6263b5e12f29e7042b905587c5e5c34c01d
Niederstadt <Malte.Niederstadt@funkemedien.de>
2023-10-30 10:38:32 +0100

    disable superuser creation

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements/dev.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py migrate --run-syncdb
+#python manage.py migrate --run-syncdb
 #python manage.py runworker websocket
 
 # if [ $CREATE_SUPERUSER ];
